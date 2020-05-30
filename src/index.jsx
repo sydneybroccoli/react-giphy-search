d7afebc97bc486c7d515928365dc04373929afd1
Sydney Barovsky <sbarovsky@gmail.com>
2020-05-31 06:13:00 +0800

    added font-awesome for real

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-import 'font-awesome/css/font-awesome.min.css';
 
 import { App } from './components/app.jsx';
 
