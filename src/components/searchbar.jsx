3155054b77dae336da9a902bd3b16731c66a4ed1
Sydney Barovsky <sbarovsky@gmail.com>
2020-05-31 07:22:41 +0800

    console prints what is being searched letter by letter

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export class SearchBar extends React.Component {
+  handleKeyUp(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      <input type="text" className="form-search form-control" />
+      <input type="text" className="form-search form-control" onKeyUp={this.handleKeyUp} />
     );
   }
 }
