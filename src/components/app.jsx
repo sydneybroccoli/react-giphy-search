5f110aa1153943e7bc8139373ca1bffdedab9a1a
Sydney Barovsky <sbarovsky@gmail.com>
2020-05-31 07:16:18 +0800

    updated app to contain gifs in state rather than props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,22 +5,29 @@ import { GifList } from './gif_list.jsx';
 import { Gif } from './gif.jsx';
 
 export class App extends React.Component {
-  render() {
-    const gifs = [
-      { id: 'xTiTnB1aEUVOtf7bQA' },
-      { id: 'VqabblvEyrHby' }
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [
+        { id: 'xTiTnB1aEUVOtf7bQA' },
+        { id: 'VqabblvEyrHby' }
 
+      ],
+      activeGif: { id: 'xTiTnB1aEUVOtf7bQA' }
+    };
+  }
+
+  render() {
     return(
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="active-gif">
-            <Gif id="xTiTnB1aEUVOtf7bQA" />
+            <Gif id={this.state.activeGif.id} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs}/>
+          <GifList gifs={this.state.gifs}/>
         </div>
       </div>
     );
