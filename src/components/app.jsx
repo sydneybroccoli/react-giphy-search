7e3bdffdc009e8bbe328ffb158006ca5b9484054
Sydney Barovsky <sbarovsky@gmail.com>
2020-05-31 07:54:54 +0800

    search functionality onChange works

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import giphy from 'giphy-api';
 import { SearchBar } from './searchbar.jsx';
 import { GifList } from './gif_list.jsx';
 import { Gif } from './gif.jsx';
@@ -15,18 +16,29 @@ export class App extends React.Component {
       ],
       activeGif: { id: 'xTiTnB1aEUVOtf7bQA' }
     };
+    this.search = this.search.bind(this);
   }
 
   // API CALL
   search(query) {
-
+    giphy('SzlFIyU01Cjvs0gI1dKIz3lv0y8eMWdq').search(
+    {
+      q: query,
+      rating: 'pg-13',
+      limit: 10
+    },
+    (error, result) => {
+      this.setState({
+        gifs: result.data
+      })
+    });
   }
 
   render() {
     return(
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar query={this.search} />
           <div className="active-gif">
             <Gif id={this.state.activeGif.id} />
           </div>
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 export class SearchBar extends React.Component {
-  handleKeyUp(event) {
-    console.log(event.target.value);
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.query(event.target.value);
   }
 
   render() {
     return (
-      <input type="text" className="form-search form-control" onKeyUp={this.handleKeyUp} />
+      <input type="text" className="form-search form-control" onChange={this.handleChange} />
     );
   }
 }
