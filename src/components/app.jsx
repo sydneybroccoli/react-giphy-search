1ec8a0e5b656dcb093f3d957488f00f3842a7594
Sydney Barovsky <sbarovsky@gmail.com>
2020-05-31 06:02:48 +0800

    added font-awesome

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { SearchBar } from './searchbar.jsx';
 
-class App extends React.Component {
+export class App extends React.Component {
   render() {
     return(
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="active-gif">
 
+          </div>
+        </div>
+        <div className="right-scene">
+          <div className="gif-list">
+
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
 
+export class SearchBar extends React.Component {
+  render() {
+    return (
+      <input type="text" className="form-search form-control" />
+    );
+  }
+}
